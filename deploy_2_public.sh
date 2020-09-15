f25f18917204a74ee4de024b698100fe3819028b
simone <simone.rubinacci@stats.ox.ac.uk>
2020-09-15 17:36:58 +0200

    Publishing github-page-maker changes.

--- deploy_2_public.sh
+++ deploy_2_public.sh
@@ -6,10 +6,10 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 #git rm --cached public
-rm -rf public
+#rm -rf public
 
 # Build the project.
-hugo -t academic # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo #-t academic # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
